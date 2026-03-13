382f43be1c0ee2e79dca1f86911a6cf4d02bcbaa
Gouri Panda <gouripanda4@gmail.com>
2026-03-13 14:06:11 +0530

    add wikidata lookup and language mapping for revisions

--- client/src/components/Revisions.jsx
+++ client/src/components/Revisions.jsx
@@ -5,19 +5,82 @@ import * as d3 from "d3";
 const width = 1000;
 const height = 500;
 
+const fixedLangOption = "enwiki";
+
 export default function Revisions() {
   const svgRef = useRef();
 
-  const [selected, setSelected] = useState(["enwiki"]);
+  const [languages, setLanguages] = useState([]);
+  const [selected, setSelected] = useState([fixedLangOption]);
+  const [titlesByWiki, setTitlesByWiki] = useState({});
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  async function getQID(title, language) {
+    try {
+      const encodedTitle = encodeURIComponent(title);
+
+      const res = await fetch(
+        `https://${language}.wikipedia.org/w/api.php?action=query&titles=${encodedTitle}&prop=pageprops&format=json&origin=*`
+      );
+
+      const json = await res.json();
+      const page = Object.values(json.query?.pages || {})[0] || {};
+
+      return page.pageprops?.wikibase_item || null;
+    } catch {
+      return null;
+    }
+  }
+
+  async function loadLanguages(article = "India", language = "en") {
+    try {
+      const qid = await getQID(article, language);
+
+      if (!qid) {
+        setLanguages([fixedLangOption]);
+        setTitlesByWiki({ [fixedLangOption]: article });
+        return;
+      }
+
+      const res = await fetch(
+        `https://www.wikidata.org/wiki/Special:EntityData/${qid}.json`
+      );
+
+      const json = await res.json();
+
+      const sitelinks = Object.entries(
+        json.entities?.[qid]?.sitelinks || {}
+      );
+
+      const langs = sitelinks.map(([wiki]) => wiki);
+
+      const mappedTitles = sitelinks.reduce((acc, [wiki, info]) => {
+        if (info?.title) acc[wiki] = info.title;
+        return acc;
+      }, {});
+
+      if (!mappedTitles[fixedLangOption]) {
+        mappedTitles[fixedLangOption] = article;
+      }
+
+      setLanguages([fixedLangOption, ...langs]);
+      setTitlesByWiki(mappedTitles);
+    } catch {
+      setLanguages([fixedLangOption]);
+      setTitlesByWiki({ [fixedLangOption]: article });
+    }
+  }
 
   async function fetchLanguage(lang) {
     try {
       const code = lang.replace("wiki", "");
+      const title = titlesByWiki[lang] || "India";
 
       const res = await fetch(
-        `https://${code}.wikipedia.org/w/api.php?action=query&prop=revisions&titles=India&rvprop=size|tags&rvlimit=50&format=json&origin=*`
+        `https://${code}.wikipedia.org/w/api.php?action=query&prop=revisions&titles=${encodeURIComponent(
+          title
+        )}&rvprop=size|tags&rvlimit=50&format=json&origin=*`
       );
 
       const json = await res.json();
@@ -51,8 +114,12 @@ export default function Revisions() {
   }
 
   useEffect(() => {
-    loadData();
-  }, [selected]);
+    loadLanguages();
+  }, []);
+
+  useEffect(() => {
+    if (languages.length) loadData();
+  }, [selected, languages]);
 
   useEffect(() => {
     if (!data.length) return;
@@ -67,16 +134,14 @@ export default function Revisions() {
 
     const rScale = d3.scaleSqrt()
       .domain([0, maxSize])
-      .range([30, 90]);
+      .range([35, 100]);
 
     const nodes = data.map((d) => ({
       ...d,
       r: rScale(d.size),
     }));
 
-    const g = svg.append("g");
-
-    const node = g.selectAll("g")
+    const node = svg.selectAll("g")
       .data(nodes)
       .enter()
       .append("g");
@@ -93,7 +158,7 @@ export default function Revisions() {
 
     const simulation = d3.forceSimulation(nodes)
       .force("center", d3.forceCenter(centerX, centerY))
-      .force("collision", d3.forceCollide().radius(d => d.r + 5))
+      .force("collision", d3.forceCollide().radius(d => d.r + 10))
       .on("tick", () => {
         node.attr("transform", d => `translate(${d.x},${d.y})`);
       });
