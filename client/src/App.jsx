fa54a481de28302d2523b41e32ef9b85eb73f34a
Gouri Panda <gouripanda4@gmail.com>
2026-04-15 08:05:37 +0530

    typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <SearchStateProvider>
       <div className="main-container">
-        <section className="title-text margin-top-3">Article overview</section>
+        <section className="title-text margin-top-3">Article Overview</section>
         <div className="controls-container">
           <section className="controls margin-top-3">
             <LanguageSearch name="language" />
